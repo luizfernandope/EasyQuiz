951f8aba0378f206f317eff364353959aaf60da7
gomeszk10 <gamerbrasileirototal@gmail.com>
2025-11-26 16:33:38 -0300

    URL NOVA

--- BackEnd_EasyQuiz/src/main/java/com/easyquiz/demo/config/WebConfig.java
+++ BackEnd_EasyQuiz/src/main/java/com/easyquiz/demo/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                     "http://localhost:3000",              
-                    "https://easyquiz-psi.vercel.app"       
+                    "https://easyquiz-chi.vercel.app"       
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
